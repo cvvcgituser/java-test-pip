7eb77dc76a23d15ce21d502d4a957b5cc7af934d
cvvcgituser <115985101+cvvcgituser@users.noreply.github.com>
2022-12-27 12:47:10 +0530

    Update Status.java

--- src/main/java/com/miguno/Status.java
+++ src/main/java/com/miguno/Status.java
@@ -1,11 +1,10 @@
 package com.miguno;
 
 import javax.ws.rs.GET;
-import javax.ws.rs.Path;
+//import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-@Path("status")
 public class Status {
 
   @GET
